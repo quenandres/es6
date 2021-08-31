f32611c470fcd1692cb4244a614a5a166d65a8e6
Jhonatan Mejia <quenandres@gmail.com>
2021-08-31 18:10:56 -0500

    Clases, Módulos y Generadores5/14

---
+++ src/es6/index.js
@@ -0,0 +1,34 @@
+class calculator {
+    constructor(){
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    sum(valueA, valueB){
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+
+}
+const calc = new calculator();
+console.log(calc.sum(2, 2));
+
+import hello from './module'
+
+hello();
+
+//Generators
+
+function* helloWorld() {
+    if( true ) {
+        yield 'Hello, ';
+    }
+    if( true ) {
+        yield 'World';
+    }
+};
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
